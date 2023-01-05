acb8cc3aa8138ea814aa640f148caa405527b04e
Andre Brdoch <andrebrdoch@yhahoo.de>
2023-01-05 11:39:00 +0100

    feat: Support without shadow root

--- Component.js
+++ Component.js
@@ -1,19 +1,27 @@
 export class Component extends HTMLElement {
-  constructor() {
+  constructor({ hasShadow = true } = {}) {
     super();
 
     const props = this._getPropsFromAttributes();
-    const shadow = this.attachShadow({ mode: 'open' });
     const html = this.template(props);
-    if (html.length) {
-      const css = this.css();
-      const style = css.length ? `<style>${css}</style>` : '';
-      const combined = html + style;
-      // remove surrounding empty spaces
-      shadow.innerHTML = combined
-        .replace(/^[\s\n\t]*/, '')
-        .replace(/[\s\n\t]*$/, '');
+
+    if (!html.length) return;
+
+    // element to apply HTML on
+    let target = this;
+    if (hasShadow) {
+      const shadow = this.attachShadow({ mode: 'open' });
+      target = shadow;
     }
+
+    const css = this.css();
+    const style = css.length ? `<style>${css}</style>` : '';
+    const combined = html + style;
+    const combinedTrimmed = combined
+      .replace(/^[\s\n\t]*/, '')
+      .replace(/[\s\n\t]*$/, '');
+
+    target.innerHTML = combinedTrimmed;
   }
 
   /** Returns the components innerHTML contents. */
--- components/IconChevron.js
+++ components/IconChevron.js
@@ -2,20 +2,14 @@ import { Component } from '../Component.js';
 
 export class IconChevron extends Component {
   constructor() {
-    super();
+    super({ hasShadow: false });
   }
 
   template({ title, content }) {
     return `
-<svg class="chevron" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
     <path fill="currentColor" d="M16.59 8.59 12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z"></path>
 </svg>
     `;
-  }
-
-  css() {
-    return `
-
-`;
   }
 }
