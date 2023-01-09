bef80ec5fd7573c5972c7cf5e90687433cdd5805
Andre Brdoch <andrebrdoch@yhahoo.de>
2023-01-09 15:37:44 +0100

    feat: Add more icons

--- register.js
+++ register.js
@@ -4,10 +4,16 @@ import { AccordionItem } from './components/AccordionItem.js';
 import { IconChevron } from './components/IconChevron.js';
 import { IconCode } from './components/IconCode.js';
 import { IconSubDirectory } from './components/IconSubDirectory.js';
+import { IconComponent } from './components/IconComponent.js';
+import { IconHtml } from './components/IconHtml.js';
+import { IconShadow } from './components/IconShadow.js';
 
 customElements.define('app-component', AppComponent);
 customElements.define('accordion-list', AccordionList);
 customElements.define('accordion-item', AccordionItem);
 customElements.define('icon-chevron', IconChevron);
 customElements.define('icon-code', IconCode);
+customElements.define('icon-component', IconComponent);
+customElements.define('icon-html', IconHtml);
+customElements.define('icon-shadow', IconShadow);
 customElements.define('icon-sub-directory', IconSubDirectory);
