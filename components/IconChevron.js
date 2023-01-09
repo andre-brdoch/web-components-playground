246208ce0e9275989cee2f3172c02f5d8cae0488
Andre Brdoch <andrebrdoch@yhahoo.de>
2023-01-09 12:23:17 +0100

    refactor: Remove unused props

--- components/IconChevron.js
+++ components/IconChevron.js
@@ -5,7 +5,7 @@ export class IconChevron extends Component {
     super({ hasShadow: false });
   }
 
-  template({ title, content }) {
+  template() {
     return `
 <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
     <path fill="currentColor" d="M16.59 8.59 12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z"></path>
--- components/IconCode.js
+++ components/IconCode.js
@@ -5,7 +5,7 @@ export class IconCode extends Component {
     super({ hasShadow: false });
   }
 
-  template({ title, content }) {
+  template() {
     return `
 <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
   <path fill="currentColor" d="M9.4 16.6 4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0 4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z"></path>
